3fdf71a8565d4eb6b9b2bb4fc194dee3173696ad
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2024-07-08 04:11:19 -0400

    task/logging

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-import os
-from dotenv import load_dotenv
+import settings
 import discord
+import requests
 from discord.ext import commands
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+logger = settings.logging.getLogger("bot")
+
+def fetch_daily_problem():
+    url = 'https://alfa-leetcode-api.onrender.com//daily'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    return None
+
+def run():
+    intents = discord.Intents.default()
+    bot = commands.Bot(command_prefix='!', intents=intents)
+
+    @bot.event
+    async def on_ready():
+        logger.info(f"User: {bot.user}")
+
+    bot.run(settings.TOKEN, root_logger=True)
+
+
+if __name__ == "__main__":
+    run()
